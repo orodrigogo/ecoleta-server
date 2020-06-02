419efb3986db6ffd8e2d0db87aa853d73823db2d
rodrigo <rodrigo.rgtic@gmail.com>
2020-06-02 16:53:58 -0300

    chore: cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import path from 'path';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
