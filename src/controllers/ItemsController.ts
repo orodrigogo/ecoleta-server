f89e6b38ca4f9ef5d9884ef820095edd1ca3961b
rodrigo <rodrigo.rgtic@gmail.com>
2020-06-10 19:02:46 -0300

    feat: upload image and serialized image for access mobile

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -11,7 +11,7 @@ class ItemsController {
     return {
       id: item.id,
       title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
+      image_url: `http://192.168.1.5:3333/uploads/${item.image}`
     };
   });
 
--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -17,6 +17,15 @@ class PointsController {
                         .distinct()
                         .select('points.*'); // Para trazer todos os campos da tabela points. E não da que eu fiz o Join.
 
+
+
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.1.12:3333/uploads/${point.image}`,
+      };
+    });
+
     return response.json(points);
   }
 
@@ -29,13 +38,24 @@ class PointsController {
       return response.status(400).json({ message: 'Point not found.'})
 
 
+    const serializedPoint = {      
+        ...point,
+        image_url: `http://192.168.1.5:3333/uploads/${point.image}`,
+      };
+
+
+
     const items = await knex('items')
                         .join('point_items', 'items.id', '=', 'point_items.item_id')
                         .where('point_items.point_id', id)
                         .select('items.title');
 
+
+
+                        
+
     return response.json({
-      point,
+      point: serializedPoint,
       items
     });
   }
@@ -58,7 +78,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -76,7 +96,10 @@ class PointsController {
     const point_id = insertedIds[0];
   
     // Registrando na tabela de relacionamento N:N.
-    const pointItems = items.map((item_id: number) => {
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
       return {
         item_id,
         point_id
